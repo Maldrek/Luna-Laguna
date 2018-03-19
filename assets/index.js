44a1f24f6be5e76a1fd619fe3bedeae9a2fa64ae
Maldrek <scott.salem5@gmail.com>
2018-03-19 12:56:48 -0500

    saving after adding sound file

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+// -------------------------------- Sound Call ---------------------------------- //
+$(document).ready(function () {
+    let audio = new Audio("../assets/sounds/Saint-Peters-Greeting.mp3");
+    audio.volume = 1.5;
+    audio.play();
+});
